294dd217bc3da77eb2354106765ea161e788ba2a
anurag2775 <vanurag177@gmail.com>
2026-01-23 16:40:32 +0530

    added json server

--- WIPRO/React Program/Day2/vite-react-app/src/components/CardSection.jsx
+++ WIPRO/React Program/Day2/vite-react-app/src/components/CardSection.jsx
@@ -1,7 +1,16 @@
-import Card from './Card'
-import data from '../data/db.json'
+import { useEffect, useState } from "react";
+import Card from "./Card";
 
 function CardSection() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/courses")
+      .then(res => res.json())
+      .then(data => setCourses(data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <section className="w-full py-12 p-6">
       <h2 className="text-2xl font-bold mb-6 text-center">
@@ -9,9 +18,9 @@ function CardSection() {
       </h2>
 
       <div className="grid md:grid-cols-3 gap-6">
-        {data.courses.map((item, index) => (
+        {courses.map((item) => (
           <Card
-            key={index}
+            key={item.id}
             title={item.title}
             description={item.description}
           />
@@ -22,4 +31,5 @@ function CardSection() {
 }
 
 export default CardSection;
+
 
