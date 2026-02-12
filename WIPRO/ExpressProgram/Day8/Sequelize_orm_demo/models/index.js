e447458b9dd90e96ee41b612c49f8201c0663bc4
anurag2775 <vanurag177@gmail.com>
2026-02-12 23:36:26 +0530

    added Day8 folder in ExpressProgram

---
+++ WIPRO/ExpressProgram/Day8/Sequelize_orm_demo/models/index.js
@@ -0,0 +1,15 @@
+const sequelize =require("../db/connection.js");
+const Course = require("./Course.js");
+const Instructor = require("./Instructor.js");
+const Enrollment = require("./Enrollment.js");
+const Studentss = require("./Studentss.js");
+Instructor.hasMany(Course,{
+    foreignKey:"instructorId",onDelete:"CASCADE"
+})
+Course.belongsTo(Instructor,{
+    foreignKey:"instructorId"
+})
+Studentss.belongsToMany(Course,{through:Enrollment})
+Course.belongsToMany(Studentss,{through:Enrollment})
+module.exports={sequelize,Course,Instructor,Studentss,Enrollment};
+
